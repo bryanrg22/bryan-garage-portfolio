457b794d38f44aa8f043307dc7293a55fa899d8f
Bryan Ramirez-Gonzalez <bryanram2024@gmail.com>
2026-02-19 00:16:37 -0800

    Switch to official @posthog/react provider with apiKey init

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
+import { PostHogProvider } from '@posthog/react'
 import './index.css'
 import App from './App'
 
 // Side-effect: initializes PostHog if VITE_POSTHOG_KEY is set
 import './lib/analytics'
 
+const options = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com',
+} as const
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PostHogProvider client={posthog}>
+    <PostHogProvider apiKey={import.meta.env.VITE_POSTHOG_KEY} options={options}>
       <App />
     </PostHogProvider>
   </StrictMode>,
